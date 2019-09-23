5a41b551b62962fac48430b1dfaf8ecfa737adbe
jfescobar18 <jfescobar18@hotmail.com>
2019-09-23 02:28:19 -0500

    about us added

--- assets/common/js/vue/components/about.js
+++ assets/common/js/vue/components/about.js
@@ -2,6 +2,30 @@ var about = Vue.component('about', {
     template: `
         <div>
             <navbar></navbar>
+            <slider></slider>
+            <div class="slider-separator"></div>
+            <div class="about-us">
+                <h1>Historia</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto.
+                </p>
+                <hr />
+                <h1>Mision</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto.
+                </p>
+                <hr />
+                <h1>Vision</h1>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Eligendi necessitatibus quis eaque doloremque, repellendus blanditiis commodi eius sequi alias, amet, aspernatur quibusdam in dolorem? Iusto facere quod repudiandae consequatur! Architecto.
+                </p>
+            </div>
         </div>
     `
 });
